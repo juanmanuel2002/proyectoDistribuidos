f96d8bb2bc154b7feafd1ebd33e64e3ee58c33bf
juanmanuel2002 <juanmanuelmirandarayon@gmail.com>
2024-11-09 20:49:38 -0600

    actualizando nombre

--- PruebaD/Prueba_Proyecto/mongo/init-mongo.js
+++ PruebaD/Prueba_Proyecto/mongo/init-mongo.js
@@ -9,6 +9,6 @@ if(!db.getCollectionNames().includes('usuarios')){
 
     db.usuarios.insertOne({
         "usuario": "admin",
-        "password": "scrypt:32768:8:1$mKvwBydkSknMheHv$97c8a899e32910d9884e39f393fd2df74b41ac5679d87768133ce229645cc5e41c0d8b7244579e3d6a91741143762cb2443555b27bc0fd49c9f273fe9e3a7083"
+        "contrasena": "scrypt:32768:8:1$mKvwBydkSknMheHv$97c8a899e32910d9884e39f393fd2df74b41ac5679d87768133ce229645cc5e41c0d8b7244579e3d6a91741143762cb2443555b27bc0fd49c9f273fe9e3a7083"
     });
 }
